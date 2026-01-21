3a688211b14b647d5f4c8435c9f7d5a2e992f8cc
khiraki <129939301+Keisuke-Hiraki@users.noreply.github.com>
2026-01-21 12:46:45 +0900

    Add action click handler for popup

---
+++ background.js
@@ -0,0 +1,14 @@
+chrome.action.onClicked.addListener(() => {
+  chrome.action.setPopup({ popup: "popup.html" }, () => {
+    const openError = chrome.runtime.lastError;
+    if (openError) {
+      chrome.tabs.create({ url: chrome.runtime.getURL("popup.html") });
+      return;
+    }
+    chrome.action.openPopup(() => {
+      if (chrome.runtime.lastError) {
+        chrome.tabs.create({ url: chrome.runtime.getURL("popup.html") });
+      }
+    });
+  });
+});
